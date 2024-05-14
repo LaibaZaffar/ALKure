d4b3c17fda65b56eb426cdfc3797bbd61ce19753
LaibaZaffar <zlaiba585@gmail.com>
2024-05-14 20:34:42 +0500

    Adding User authentication via sessions.

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,22 +1,19 @@
-
-//this file will handle the requests related to my pages
-
 // controllers/pageController.js
 const path = require('path');
 
 // Render landing page
-exports.getLayoutPage = (req, res) => {
-    res.render(path.join(__dirname, '../views/layout'));   //.. means exit 2 steps back in the path
+exports.getLandingPage = (req, res) => {
+    const sessionData = req.session;
+    res.render(path.join(__dirname, '../views/landing'), { sessionInfo: sessionData }); 
 };
 
 // Render forms page
-exports.getIndexPage = (req, res) => {
-    res.render(path.join(__dirname, '../views/index'));
+exports.getLoginPage = (req, res) => {  
+    res.render(path.join(__dirname, '../views/login'));
 };
 
 // Render forms page
 exports.getHomePage = (req, res) => {
-    res.render(path.join(__dirname, '../views/home'));
+    const sessionData = req.session;
+    res.render(path.join(__dirname, '../views/home'), { sessionInfo: sessionData }); 
 };
-
-
